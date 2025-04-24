31bcadd9a0fd42e3b0e0e454b9db90f2177b0805
naaga <naga472@gmail.com>
2025-04-24 16:19:04 +0530

    Add files via upload

---
+++ AzureStorageResource.tf
@@ -0,0 +1,8 @@
+resource "azurerm_storage_account" "sa1" {
+  name                     = "<demo1_storage_account_name>"
+  resource_group_name      = "TerraformRG"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
